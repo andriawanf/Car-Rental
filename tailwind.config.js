bdce1e86bc068a12b084ab2d750f91b55cde6cb8
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2022-07-30 11:41:34 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors:{
+        primary: '#F1F1F2',
+        secondary: '#0F0F0F',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1440px',
+      },
+    },
+  },
+  plugins: [],
+}
